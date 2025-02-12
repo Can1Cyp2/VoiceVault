46d4f4cafe0b9838cd2d5d57338156a118cb6aba
Sebastian L <99sebastianl@gmail.com>
2025-02-12 18:13:39 -0500

    Updated how users are deleted

--- app/util/deleteUser.ts
+++ app/util/deleteUser.ts
@@ -1,3 +1,8 @@
-/* -- Find all deleted users (manual cleanup)
-SELECT * FROM auth.users WHERE raw_user_meta_data->>'deleted' = 'true';
+/* 
+
+delete from auth.users
+where id in (
+   select id from auth.users where raw_user_meta_data->>'deleted' = 'true'
+);
+
 */

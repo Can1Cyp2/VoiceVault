e6d9014884c7ab6340ff41bdc41b8904636f2a0d
Sebastian L <99sebastianl@gmail.com>
2025-08-09 13:59:01 -0400

    Updated song count in home screen to reflect real song count from backend

--- app/screens/HomeScreen/HomeScreen.tsx
+++ app/screens/HomeScreen/HomeScreen.tsx
@@ -157,7 +157,7 @@ export default function HomeScreen({ navigation }: HomeScreenProps) {
       <Text style={styles.title}>Welcome to VoiceVault!</Text>
       <Text style={styles.subtitle}>
         Explore the world of vocal ranges and discover music like never before,
-        with over 28,000 songs!
+        with over 30,000 songs!
       </Text>
       {isLoggedIn ? (
         <TouchableOpacity style={styles.button} onPress={handleLogout}>
